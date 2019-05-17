02058f22703f7106edae64cb27dccdeff2571d3b
unknown <marcelomariduena@protonmail.com>
2019-05-17 12:01:36 -0400

    Updates

---
+++ precache-manifest.5832b39b4ebb844241ce1734a52dfcd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907858e09c706f67acf38ebb18d471c2",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "c9cca93a5f3ead248941",
+    "url": "/ReactCalculator/static/css/main.685cc913.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ReactCalculator/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c9cca93a5f3ead248941",
+    "url": "/ReactCalculator/static/js/main.ebd25bef.chunk.js"
+  },
+  {
+    "revision": "236c54c985da26a52bc6",
+    "url": "/ReactCalculator/static/js/runtime~main.70542196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactCalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
